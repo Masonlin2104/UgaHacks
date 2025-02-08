1b591fe045260c1a5426a31fe96014423eb7ffc1
Lera <vl47623@uga.edu>
2025-02-07 23:25:23 -0500

    added some frontend stuff for fun

--- src/App.jsx
+++ src/App.jsx
@@ -16,38 +16,35 @@ function App() {
       <div>
         <img src={musicNote} className="music note" alt="music note" style={{ height: 70, width: 70 }}/>
       </div>
-      <h1>LoLdle (Music version hehe)</h1>
+      <h2>Songle</h2>
       <div className="card">
-        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-          <div>
+      <div class="lyrics-card">
             <h2>Lyrics:</h2>
             <p>{lyrics}</p>
           </div>
-          <div>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <div class="category-card">
             <h2>Genre:</h2>
             <p>{genre}</p>
           </div>
-          <div>
+          <div class="category-card">
             <h2>Year:</h2>
             <p>{year}</p>
           </div>
-          <div>
+          <div class="category-card">
             <h2>Artist:</h2>
             <p>{artist}</p>
           </div>
         </div>
-        <form>
+        <form class="input-form">
           <label>
             Guess the Song: 
             <input type="text" name="name" />
           </label>
         </form>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p className="subtitle">
+        UGAHacksX - 2025
       </p>
     </>
   )
